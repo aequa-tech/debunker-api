fbb05f04edc981d83916295d7222e12c3c0a562f
Daniele Giorgis <daniele.giorgis@welaika.com>
2024-04-05 17:49:48 +0200

    Add User model

---
+++ db/migrate/20240405153718_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.string :confirmation_token
+      t.string :confirmed_at
+      t.string :confirmation_sent_at
+      t.string :api_key
+
+      t.timestamps
+    end
+  end
+end
