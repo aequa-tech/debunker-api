6d9aba59495081537a10e98f5025caaa1803f67b
Daniele Giorgis <daniele.giorgis@welaika.com>
2024-04-07 10:29:12 +0200

    Rubocop

--- test/controllers/registrations_controller_test.rb
+++ test/controllers/registrations_controller_test.rb
@@ -2,7 +2,7 @@
 
 require 'test_helper'
 
-class UserTest < ActiveSupport::TestCase
+class RegistrationsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
