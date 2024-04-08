e85927b8218f7b5d60951617e1f432e865929afd
Daniele Giorgis <daniele.giorgis@welaika.com>
2024-04-08 14:46:25 +0200

    Devolpment (mailhog) mail interceptor

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,6 +40,15 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
+
+  # In development we use mailhog to catch emails
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'localhost',
+    port: 1025
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
