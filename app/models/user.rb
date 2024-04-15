2d54f6a4cd65cc1737abea6b1bb5644e48ef66b3
Daniele Giorgis <daniele.giorgis@welaika.com>
2024-04-15 16:15:32 +0200

    Align models from web

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,18 @@
 
 class User < ApplicationRecord
   has_many :tokens, dependent: :destroy
+  has_many :api_keys, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true
 
+  enum role: %i[user admin]
+
   def info_attributes
     attributes.slice('name', 'email', 'api_key')
+  end
+
+  def available_tokens
+    tokens.available
   end
 end
