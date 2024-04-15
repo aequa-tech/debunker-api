9c18baacf3ca702eb42cc91ece1d7fa327b39018
Daniele Giorgis <daniele.giorgis@welaika.com>
2024-04-15 16:36:59 +0200

    Update on user status api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,10 +6,7 @@ module Api
       def status
         render json: {
           user: @user.info_attributes.except('api_key'),
-          tokens: {
-            count: @user.tokens.count,
-            list: @user.tokens.map(&:value)
-          }
+          available_tokens: @user.tokens.count
         }, status: :ok
       end
     end
