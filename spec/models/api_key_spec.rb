e15ac241a7ac18a2af5bab2f6313a3f26958d55f
Daniele Giorgis <daniele.giorgis@welaika.com>
2024-06-28 09:01:58 +0200

    Fix specs

--- spec/models/api_key_spec.rb
+++ spec/models/api_key_spec.rb
@@ -66,6 +66,8 @@ RSpec.describe ApiKey, type: :model do
   end
 
   describe 'create a new api key' do
+    before { api_key }
+
     it 'create tokens from tier' do
       expect(api_key.tokens.available.count).to eq(user.role.tier.tokens_rate)
       expect(api_key.reloaded_at).to eq(Date.today)
